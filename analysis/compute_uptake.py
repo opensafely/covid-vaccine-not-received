59db6381208da34edf4cf5c191e2259e16d1a68e
HelenCEBM <helencebm@gmail.com>
2021-04-21 19:55:05 +0100

    Update compute_uptake.py
    Remove unnecessary function

--- analysis/compute_uptake.py
+++ analysis/compute_uptake.py
@@ -4,34 +4,6 @@ import pandas as pd
 from groups import groups
 
 
-def run(input_path="output/cohort.pickle", output_dir="output"):
-    """Produce dataframes computing uptake stratified by age band, sex, and high-level
-    ethnicity codes, for the whole population and for each clinical subgroup in the
-    PRIMIS spec.
-    """
-
-    backend = os.getenv("OPENSAFELY_BACKEND", "expectations")
-
-    cohort = pd.read_pickle(input_path)
-    demographic_cols = ["age_band", "sex", "high_level_ethnicity"]
-    group_cols = [
-        group for group in groups if "covax" not in group and "unstatvacc" not in group
-    ]
-
-    for col in demographic_cols:
-        path = f"{output_dir}/coverage_by_{col}_{backend}.csv"
-        compute_uptake(cohort, "vacc1_dat", col).to_csv(path)
-
-        for group_col in group_cols:
-            group_cohort = cohort[cohort[group_col]]
-            path = f"{output_dir}/coverage_for_{group_col}_by_{col}_{backend}.csv"
-            compute_uptake(group_cohort, "vacc1_dat", col).to_csv(path)
-
-    for col in group_cols:
-        path = f"{output_dir}/coverage_by_{col}_{backend}.csv"
-        compute_uptake(cohort, "vacc1_dat", col).to_csv(path)
-
-
 def compute_uptake(cohort, event_col, stratification_col):
     stratification_series = cohort[stratification_col]
     stratification_vals = sorted(stratification_series.value_counts().index)
