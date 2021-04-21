02b15d80bf13b2665756f7cd159def99016a2550
HelenCEBM <helencebm@gmail.com>
2021-04-21 13:44:04 +0100

    Update custom_operations.py

--- analysis/custom_operations.py
+++ analysis/custom_operations.py
@@ -1,6 +1,12 @@
 import os
 import pandas as pd
 
+
+
+import os
+import csv
+import pandas as pd
+
 def practice_variation(input_path="output/cohort.pickle", output_dir="output"):
     ''' Calculates total patients per practice and of whom how many have had a vaccine to date, or declined
     '''
@@ -8,14 +14,17 @@ def practice_variation(input_path="output/cohort.pickle", output_dir="output"):
     base_path = f"{output_dir}/{backend}/coverage_to_date"
     cohort = pd.read_pickle(input_path)
 
-    practice_figures = cohort[["practice", "vacc_group", "decline_group", "patient_id"]].groupby("practice").count()
+    practice_figures = cohort[["practice", "vacc_group", "decline_group", "patient_id"]]\
+                        .groupby("practice").agg({"vacc_group":"sum", 
+                                                  "decline_group":"sum", 
+                                                  "patient_id":"nunique"})
     practice_figures = practice_figures.rename(columns={"patient_id":"patient_count"})
     
-    # remove tiny practices
+    # remove tiny practices and ensure that at least one patient has been vaccinated in each practice
     if backend=="expectations":
-        practice_figures = practice_figures.loc[practice_figures["patient_count"]>5]
+        practice_figures = practice_figures.loc[(practice_figures["patient_count"]>10)&(practice_figures["vacc_group"]>0)]
     else:
-        practice_figures = practice_figures.loc[practice_figures["patient_count"]>100]
+        practice_figures = practice_figures.loc[(practice_figures["patient_count"]>100)&(practice_figures["vacc_group"]>0)]
     
     practice_figures = practice_figures.assign(
         decline_per_1000 = 1000*practice_figures["decline_group"]/practice_figures["patient_count"],
@@ -26,4 +35,3 @@ def practice_variation(input_path="output/cohort.pickle", output_dir="output"):
 
 out = practice_variation()
 print(out.head(20))
-
