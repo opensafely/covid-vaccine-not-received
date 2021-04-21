ad85e715478460f9e321ad7712cb748a82726a42
HelenCEBM <helencebm@gmail.com>
2021-04-21 16:43:01 +0100

    Plot and export practice histogram

--- analysis/custom_operations.py
+++ analysis/custom_operations.py
@@ -1,14 +1,12 @@
 import os
 import pandas as pd
-
-
-
-import os
-import csv
-import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
 
 def practice_variation(input_path="output/cohort.pickle", output_dir="output"):
     ''' Calculates total patients per practice and of whom how many have had a vaccine to date, or declined
+    Note: those declining only include those who have not later received a vaccine.
     '''
     backend = os.getenv("OPENSAFELY_BACKEND", "expectations")
     base_path = f"{output_dir}/{backend}/coverage_to_date"
@@ -31,7 +29,26 @@ def practice_variation(input_path="output/cohort.pickle", output_dir="output"):
         decline_per_1000_vacc = 1000*practice_figures["decline_group"]/practice_figures["vacc_group"]
     )
 
-    return practice_figures
+    out = practice_figures
 
-out = practice_variation()
-print(out.head(20))
+    fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+
+    for n, x in enumerate(["decline_per_1000", "decline_per_1000_vacc"]):
+        # set weights to show percent rather than count of practices
+        weights=np.ones(len(out)) / len(out)
+        axs[n].hist(out[x], bins=15, weights=weights)
+        title = "COVID Vaccines "+ x.replace("_"," ").replace("decline", "declined\n").replace("vacc","vaccinated").title()+" Patients"
+        axs[n].set_title(title)
+        axs[n].set_xlabel("Rate per 1000")
+        axs[n].yaxis.set_major_formatter(PercentFormatter(1))
+
+    axs[0].set_ylabel("Percent of practices")
+
+    return (backend, fig)
+
+backend, fig = practice_variation()
+  
+fig.savefig(f"output/{backend}/declines_by_practice_hist.png")
+
+
+
