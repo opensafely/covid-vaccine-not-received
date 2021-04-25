05c92e1bd26a6a6b59acb3d1a59270664f75d628
HelenCEBM <helencebm@gmail.com>
2021-04-25 20:56:55 +0100

    Update generate_all_charts_and_tables.py
    Fix path

--- analysis/generate_all_charts_and_tables.py
+++ analysis/generate_all_charts_and_tables.py
@@ -1,7 +1,9 @@
 from generate_paper_outputs import run
 from custom_operations import practice_variation, current_variation, declined_vaccinated
+import os
 
-base_path = f"output/expectations"
+backend = os.getenv("OPENSAFELY_BACKEND", "expectations")
+base_path = f"output/{backend}"
 start_date = "2020-12-01"
 end_date = "2021-04-14"
 
