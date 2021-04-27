11ab213b4e2e94cbad707d9757756eb67dd3d8b0
HelenCEBM <helencebm@gmail.com>
2021-04-27 10:36:44 +0100

    create overview counts

--- analysis/count_prevalences.py
+++ analysis/count_prevalences.py
@@ -1,40 +1,53 @@
 import os
-
 import pandas as pd
 
 
-def run(input_path="output/cohort.pickle", output_path="output/prevalences.pickle"):
+def run(input_path="output/cohort.pickle"):
     backend = os.getenv("OPENSAFELY_BACKEND", "expectations")
-    output_path = output_path[:-7] + f"_{backend}" + ".pickle"
+    output_path = "output/" + backend + "/tables/prevalences.csv"
 
     cohort = pd.read_pickle(input_path)
     prevalences = count_prevalences(cohort)
-    prevalences.to_pickle(output_path)
-
+    prevalences.to_csv(output_path)
 
 def count_prevalences(cohort):
     prevalences = pd.DataFrame(
-        {"total": cohort.groupby(["age_band", "sex"])["patient_id"].count()}
+        {"total": cohort.groupby(["wave"])["patient_id"].count()}
     )
 
-    for col in cohort.columns:
-        if not col.endswith("_group"):
-            continue
-
+    pop_total = cohort["patient_id"].count()
+    
+    cols = ["vacc_group", "decline_group","other_reason_group", "declined_accepted_group", "preg_group"]
+    
+    for col in cols:
         prevalences[col] = (
-            cohort[cohort[col]].groupby(["age_band", "sex"])["patient_id"].count()
+            cohort[cohort[col]].groupby(["wave"])["patient_id"].count()
         )
+
+    totals = cohort[cols].sum()
+    totals["total"] = pop_total
+    totals = totals.rename("total")
+    prevalences = prevalences.append(totals)
 
     for high_level_ethnicity_category in [1, 2, 3, 4, 5, 6]:
         prevalences[f"ethnicity_{high_level_ethnicity_category}"] = (
             cohort[cohort["high_level_ethnicity"] == high_level_ethnicity_category]
-            .groupby(["age_band", "sex"])["patient_id"]
+            .groupby(["wave"])["patient_id"]
             .count()
         )
 
+        eth_total = (
+            cohort[cohort["high_level_ethnicity"] == high_level_ethnicity_category]
+            ["patient_id"].count()
+        )
+
+        prevalences[f"ethnicity_{high_level_ethnicity_category}"].loc["total"] = eth_total
+
+    prevalences.fillna(0, inplace=True)
+
+    for c in prevalences.columns:
+        prevalences[f"{c}_percent"] = 100*prevalences[c]/prevalences["total"]
     prevalences.fillna(0, inplace=True)
     return ((prevalences // 7) * 7).astype(int)
 
-
-if __name__ == "__main__":
-    run()
+run()
