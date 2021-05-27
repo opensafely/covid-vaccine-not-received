e82f794012e7a5769e1b0afdd10c89cac3040136
HelenCEBM <helencebm@gmail.com>
2021-05-27 10:00:26 +0100

    Update run_charts_and_tables.py
    Update end date

--- analysis/run_charts_and_tables.py
+++ analysis/run_charts_and_tables.py
@@ -4,7 +4,7 @@ import os
 backend = os.getenv("OPENSAFELY_BACKEND", "expectations")
 base_path = f"output/{backend}"
 start_date = "2020-12-01"
-end_date = "2021-05-05"
+end_date = "2021-05-20"
 
 # create cumulative charts
 run(base_path, start_date, end_date)
