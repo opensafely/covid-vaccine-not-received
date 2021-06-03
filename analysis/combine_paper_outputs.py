d956e16726eb18e802b8d17f1826046bb3c64aea
HelenCEBM <helencebm@gmail.com>
2021-06-03 14:40:09 +0100

    Combined outputs

---
+++ analysis/combine_paper_outputs.py
@@ -0,0 +1,36 @@
+import glob
+import os
+import pandas as pd
+
+from combine_cumsums import combine_cumsums
+
+
+def run():
+    # for emis_path in sorted(
+    #     glob.glob("released_outputs/emis/cumulative_coverage/*/*/*.csv")
+    # ):
+    #     print(emis_path)
+    #     tpp_path = emis_path.replace("/emis/", "/tpp/")
+    #     emis_df = pd.read_csv(emis_path, index_col=0)
+    #     tpp_df = pd.read_csv(tpp_path, index_col=0)
+    #     combined_df = combine_cumsums(emis_df, tpp_df)
+    #     combined_path = emis_path.replace("/emis/", "/combined/")
+    #     os.makedirs(os.path.dirname(combined_path), exist_ok=True)
+    #     combined_df.to_csv(combined_path)
+
+    # combine additional tables
+    for emis_path in sorted(
+        ["released_outputs/emis/tables/prevalences.csv",
+        "released_outputs/emis/additional_figures/practice_decline_summary.csv",
+        ]
+    ):
+        print(emis_path)
+        tpp_path = emis_path.replace("/emis/", "/tpp/")
+        emis_df = pd.read_csv(emis_path, index_col=0)
+        tpp_df = pd.read_csv(tpp_path, index_col=0)
+        combined_df = emis_df + tpp_df
+        combined_path = emis_path.replace("/emis/", "/combined/")
+        os.makedirs(os.path.dirname(combined_path), exist_ok=True)
+        combined_df.to_csv(combined_path)
+
+run()
