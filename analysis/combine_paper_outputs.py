1c494b56fa3b4ca4af1f8801d458244c4197d546
HelenCEBM <helencebm@gmail.com>
2021-06-25 12:11:31 +0100

    Combine declined-accepted data

--- analysis/combine_paper_outputs.py
+++ analysis/combine_paper_outputs.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import pandas as pd
+import numpy as np
 
 pd.options.display.float_format = "{:,.2f}".format
 
@@ -9,28 +10,37 @@ from combine_cumsums import combine_cumsums
 def run():
     # combine cumulative sums
     for emis_path in sorted(
-           glob.glob("released_outputs/emis/cumulative_coverage/*/*/*.csv")
-       ):
+          glob.glob("released_outputs/emis/cumulative_coverage/*/*/*.csv")
+      ):
        combine(emis_path, cumsums=True)
 
     # combine other files
     for emis_path in ["released_outputs/emis/tables/prevalences.csv",
-            "released_outputs/emis/additional_figures/practice_decline_summary.csv"]:
+            "released_outputs/emis/additional_figures/practice_decline_summary.csv",
+            "released_outputs/emis/additional_figures/declined_then_accepted.csv"]:
         combine(emis_path, cumsums=False)
+
+    for emis_path in ["released_outputs/emis/additional_figures/declined_then_accepted_by_wave.csv"]:
+        combine(emis_path, cumsums=False, indices=2)
+
     for emis_path in (
         glob.glob("released_outputs/emis/additional_figures/practice_list_size*.csv")):
         combine(emis_path, cumsums=False)
     
+
     combine_multiple_waves()
     combine_multiple_waves(breakdown="imd_band",
                         ix=["Unknown","1 (most deprived)","2","3","4","5 (least deprived)"])        
 
 
-def combine(emis_path, cumsums=True):
+def combine(emis_path, cumsums=True, indices=1):
     print(emis_path)
     tpp_path = emis_path.replace("/emis/", "/tpp/")
-    emis_df = pd.read_csv(emis_path, index_col=0)
-    tpp_df = pd.read_csv(tpp_path, index_col=0)
+
+    indices = np.arange(0,indices)
+
+    emis_df = pd.read_csv(emis_path, index_col=indices)
+    tpp_df = pd.read_csv(tpp_path, index_col=indices)
     if cumsums==True:
         combined_df = combine_cumsums(emis_df, tpp_df)
     else:
@@ -39,6 +49,11 @@ def combine(emis_path, cumsums=True):
         if "_percent" in col:
             count_col = col.replace("_percent", "")
             combined_df[col] = (100*combined_df[count_col]/combined_df["total"]).round(1)
+        if "declined_then_accepted" in emis_path: # calculate rates
+            combined_df["per_1000"] = (1000*combined_df["Declined then accepted"]/combined_df["total"]).round(2)
+            combined_df["per_1000_vacc"] = (1000*combined_df["Declined then accepted"]/combined_df["Vaccinated"]).round(2)
+            combined_df["converted"] = (1000*combined_df["Declined then accepted"]/combined_df["Declined - all"]).round(2)
+
     combined_path = emis_path.replace("/emis/", "/combined/")
     os.makedirs(os.path.dirname(combined_path), exist_ok=True)
     combined_df.to_csv(combined_path)
