460c6160daa1615d49a7c82f6b58b904a6af9863
HelenCEBM <helencebm@gmail.com>
2021-06-28 13:14:33 +0100

    Combine number of weeks delay between decline-accept

--- analysis/combine_paper_outputs.py
+++ analysis/combine_paper_outputs.py
@@ -20,7 +20,8 @@ def run():
             "released_outputs/emis/additional_figures/declined_then_accepted.csv"]:
         combine(emis_path, cumsums=False)
 
-    for emis_path in ["released_outputs/emis/additional_figures/declined_then_accepted_by_wave.csv"]:
+    for emis_path in ["released_outputs/emis/additional_figures/declined_then_accepted_by_wave.csv",
+            "released_outputs/emis/additional_figures/declined_accepted_weeks_by_wave.csv"]:
         combine(emis_path, cumsums=False, indices=2)
 
     for emis_path in (
@@ -45,6 +46,12 @@ def combine(emis_path, cumsums=True, indices=1):
         combined_df = combine_cumsums(emis_df, tpp_df)
     else:
         combined_df = emis_df + tpp_df
+
+    if "declined_accepted_weeks" in emis_path: # calculate rates
+        combined_df = combined_df.unstack()
+        combined_df.columns = combined_df.columns.droplevel()
+        print(combined_df, combined_df.sum())
+    
     for col in combined_df.columns: ### recalculate any percentages
         if "_percent" in col:
             count_col = col.replace("_percent", "")
@@ -53,6 +60,8 @@ def combine(emis_path, cumsums=True, indices=1):
             combined_df["per_1000"] = (1000*combined_df["Declined then accepted"]/combined_df["total"]).round(2)
             combined_df["per_1000_vacc"] = (1000*combined_df["Declined then accepted"]/combined_df["Vaccinated"]).round(2)
             combined_df["converted"] = (1000*combined_df["Declined then accepted"]/combined_df["Declined - all"]).round(2)
+        if "declined_accepted_weeks" in emis_path: # calculate percentages
+            combined_df[f"{col}_percent"] = (100*combined_df[col]/combined_df.sum(axis=1)).round(3)
 
     combined_path = emis_path.replace("/emis/", "/combined/")
     os.makedirs(os.path.dirname(combined_path), exist_ok=True)
