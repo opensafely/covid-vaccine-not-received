d583b9a23b8a7805e37f592cc92431d4dd17e61d
HelenCEBM <helencebm@gmail.com>
2021-07-15 17:33:47 +0100

    Uncomment function

--- analysis/run_charts_and_tables combined.py
+++ analysis/run_charts_and_tables combined.py
@@ -7,7 +7,7 @@ start_date = "2020-12-08"
 end_date = "2021-05-25"
 
 # create cumulative charts
-#run(base_path, start_date, end_date)
+run(base_path, start_date, end_date)
 
 
 # plot combined-only charts: declines broken down by priority group and ethnicity/imd
