4bd4423151868ca293dd57d8cd26d2996a0bf1ef
HelenCEBM <helencebm@gmail.com>
2021-07-15 17:31:47 +0100

    Export summary figures on declined-received group

--- analysis/generate_extra_combined_charts.py
+++ analysis/generate_extra_combined_charts.py
@@ -19,16 +19,42 @@ def plot_grouped_bar(backend="combined", output_dir="released_outputs/combined",
         width = 0.1 # the width of the bars
     
     elif measure == "declined_then_accepted" and breakdown=="high_level_ethnicity":
-        df = pd.read_csv(f"released_outputs/{backend}/additional_figures/declined_then_accepted_by_wave.csv", index_col=[0,1])
-        df = df[["converted"]].unstack()/10
+        df0 = pd.read_csv(f"released_outputs/{backend}/additional_figures/declined_then_accepted_by_wave.csv", index_col=[0,1])
+        df = df0.copy()[["converted"]].unstack()/10
         df.columns = df.columns.droplevel()
         # reorder ethnicity for consistency
         df = df[["White", "Mixed", "South Asian", "Black", "Other", "Unknown"]]
+        
         # reorder cohorts into priority order using list of cohorts
         groups = list(wave_column_headings[''].values())
         groups = [g for g in groups if g in df.index and g!="Other"]
         df = df.transpose()[groups]
+        
+        # summary statistics for south asian vs other ethnicities
+        df_summary = df0.copy()[["Declined then accepted", "Declined - all"]].swaplevel()
+        # set up df to export results
+        ind = pd.MultiIndex.from_tuples([], names=(u'Ethnicity', u'Group'))
+        results = pd.DataFrame(columns=['Declined then received', 'Declined - all', 'percent'], index=ind)
+            
+        for eth_group in ["All others", "South Asian"]:
+            if eth_group == "South Asian":
+                df_2 = df_summary.loc[eth_group] # this drops one level of index
+            else:
+                df_2 = df_summary.loc[df_summary.index != "South Asian"]
+                df_2 = df_2.groupby(level=1).sum() # sum across other ethnicities   
+        
+            # summarise data for combined priority groups:
+            regroups = {"65+":["65-69", "70-79", "80+", "In care home"], "CEV/At risk": ["CEV", "At risk"], "50-64":["50-54", "55-59", "60-64"]}
 
+            for wave_group in regroups:
+                out = df_2.loc[regroups[wave_group]].sum() # sum across priority groups
+                total1 = out["Declined then accepted"]
+                total2 = out["Declined - all"]
+                out = 100*(total1/total2).round(3)
+                # save results to dataframe
+                results.loc[(eth_group, wave_group), :] = [total1,total2, out]
+        results.to_csv(f"released_outputs/{backend}/additional_figures/declined_then_accepted_by_wave_summarised.csv", float_format="%.1f")
+        
         breakdown_title = breakdown.replace("_"," ").title().replace("Imd","IMD")
         title = f'Percent of people in each {breakdown_title}\n who had a decline recorded and were later vaccinated,\nby priority group'
         ylabel = "% later vaccinated"
